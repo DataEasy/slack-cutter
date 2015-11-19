844b742eb90c4c39f223deb125a2ddf9b048fadf
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2015-11-19 17:34:00 -0200

    Fix Babel 6 not working with proxyquire. Remove it

--- test/slash-commands/prs-test.js
+++ test/slash-commands/prs-test.js
@@ -8,12 +8,14 @@ import fs from 'fs';
 import moment from 'moment';
 
 import CustomFormatter from '../../lib/slash-commands/prs/custom-formatter';
+import PrsCommand from '../../lib/slash-commands/prs/prs';
 
 const noop = () => {};
 const dummyRes = { send: noop };
 
 describe('PRs command', () => {
     let sampleGithubResponse;
+    let prsCommand;
 
     before(() => {
         process.env.NODE_ENV = 'TEST';
@@ -26,23 +28,23 @@ describe('PRs command', () => {
 
     it('should receive at least one argument', () => {
         const fn = () => {
-            const prsCommand = require('../../lib/slash-commands/prs/prs');
-            prsCommand(dummyRes).listPrs();
+            prsCommand = new PrsCommand(dummyRes);
+            prsCommand.listPrs();
         };
         expect(fn).to.throw(/At least one argument must be passed/);
     });
 
     it('should order by oldest creation date by default', done => {
-        const prsCommand = proxyquire('../../lib/slash-commands/prs/prs', {
-            'request': (options, callback) => {
-                expect(options.url).to.contain('sort=created');
-                expect(options.url).to.contain('direction=asc');
+        const dummyReq = (options, callback) => {
+            expect(options.url).to.contain('sort=created');
+            expect(options.url).to.contain('direction=asc');
 
-                callback(null, {}, sampleGithubResponse);
-            }
-        });
+            callback(null, {}, sampleGithubResponse);
+        };
 
-        prsCommand(dummyRes).listPrs('docflow', (error, result) => {
+        prsCommand = new PrsCommand(dummyRes, dummyReq);
+
+        prsCommand.listPrs('docflow', (error, result) => {
             const temp = result.split('\n\n')[1];
             const firstLine = temp.split('\n')[0];
             const originalPrDate = moment('2015-03-25', 'YYYY-MM-DD');
@@ -57,16 +59,12 @@ describe('PRs command', () => {
     it('should use the default formatter if no custom formatter for that repo', done => {
         const formatterSpy = sinon.spy(CustomFormatter, 'defaultFormatter');
 
-        const prsCommand = proxyquire('../../lib/slash-commands/prs/prs', {
-            'request': (options, callback) => {
-                callback(null, {}, sampleGithubResponse);
-            },
-            './CustomFormatter': {
-                'defaultFormatter': formatterSpy
-            }
-        });
+        const dummyReq = (options, callback) => {
+            callback(null, {}, sampleGithubResponse);
+        };
 
-        prsCommand(dummyRes).listPrs('otherrepo', (error, result) => {
+        prsCommand = new PrsCommand(dummyRes, dummyReq);
+        prsCommand.listPrs('otherrepo', (error, result) => {
             expect(formatterSpy).to.have.been.called;
 
             done();
@@ -76,16 +74,12 @@ describe('PRs command', () => {
     it('should use custom formatters if present', done => {
         const formatterSpy = sinon.spy(CustomFormatter, 'docflow');
 
-        const prsCommand = proxyquire('../../lib/slash-commands/prs/prs', {
-            'request': (options, callback) => {
-                callback(null, {}, sampleGithubResponse);
-            },
-            './CustomFormatter': {
-                'docflow': formatterSpy
-            }
-        });
+        const dummyReq = (options, callback) => {
+            callback(null, {}, sampleGithubResponse);
+        };
 
-        prsCommand(dummyRes).listPrs('docflow', (error, result) => {
+        prsCommand = new PrsCommand(dummyRes, dummyReq);
+        prsCommand.listPrs('docflow', (error, result) => {
             expect(formatterSpy).to.have.been.called;
 
             done();
@@ -93,24 +87,22 @@ describe('PRs command', () => {
     });
 
     it('should list all open PRs by default', done => {
-        const prsCommand = proxyquire('../../lib/slash-commands/prs/prs', {
-            'request': (options, callback) => {
-                expect(options.url).to.contain('state=open');
-                done();
-            }
-        });
+        const dummyReq = (options, callback) => {
+            expect(options.url).to.contain('state=open');
+            done();
+        };
 
-        prsCommand(dummyRes).listPrs('docflow', noop);
+        prsCommand = new PrsCommand(dummyRes, dummyReq);
+        prsCommand.listPrs('docflow', noop);
     });
 
     it('should query github\'s URL based on the first argument', done => {
-        const prsCommand = proxyquire('../../lib/slash-commands/prs/prs', {
-            'request': (options, callback) => {
-                expect(options.url).to.contain('github.com/repos/dataeasy/docflow/');
-                done();
-            }
-        });
+        const dummyReq = (options, callback) => {
+            expect(options.url).to.contain('github.com/repos/dataeasy/docflow/');
+            done();
+        };
 
-        prsCommand(dummyRes).listPrs('docflow', noop);
+        prsCommand = new PrsCommand(dummyRes, dummyReq);
+        prsCommand.listPrs('docflow', noop);
     });
 });
